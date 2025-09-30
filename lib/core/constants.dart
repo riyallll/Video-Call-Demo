ef0fe368fd3ea566a5ac0fe51f9bc3d770ad516f
riyal1234 <99786117+riyal1234@users.noreply.github.com>
2025-09-30 10:13:51 +0530

    Agora Token and agoraAppId change. old one expired.

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -2,7 +2,7 @@ class AppConstants {
   // Agora Keys - REPLACE the APP ID with your own from Agora dashboard
   static const String agoraAppId = "52efbe31eedc4133a2939fb73cbc31fc";
   static const String channelName = "testChannel"; // you can keep or change
-  static const String token = "007eJxTYFDfzBWrIPNVQfaKx7M0pnmL2Lcnp2/MsN7p3rQsXOD7USEFBlOj1LSkVGPD1NSUZBNDY+NEI0tjy7Qkc+PkpGRjw7Rk05ybGQ2BjAx/HbczMEIhiM/NUJJaXOKckZiXl5rDwAAApd8hTg=="; // leave empty for now
+  static const String token = "007eJxTYAgPfdp/8/XJL6sdxKXjF4jbTF1++Nn6muKsh4kW27Yx/21XYDA1Sk1LSjU2TE1NSTYxNDZONLI0tkxLMjdOTko2NkxLlou9ndEQyMjwlCGVlZEBAkF8boaS1OIS54zEvLzUHAYGAPfCJFw=";
 
   // Notifications
   static const String notificationChannelId = "video_call_channel";
